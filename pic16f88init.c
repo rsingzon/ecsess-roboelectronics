98bcf11dae77558d5ecfabf381e95cdfaea4faf3
JDAlfaro <JoshuaDavidAlfaro@gmail.com>
2014-07-30 20:50:49 -0400

    Updated source files

---
+++ pic16f88init.c
@@ -0,0 +1,11 @@
+#include "pic16f88Init.h"
+#include <pic16f88.h>
+void initPIC16F88()
+{
+    OSCCON = 0b01101110;
+    TRISA = 0;
+    TRISB = 0;
+    ANSEL = 0;
+    PORTA = 0;
+    PORTB = 0;
+}
