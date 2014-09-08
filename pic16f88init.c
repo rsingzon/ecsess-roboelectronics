be13d77343211cacaf2388ae13eed9b637c1985f
Dirk7589 <dirk.a.dubois@gmail.com>
2014-09-07 21:53:02 -0400

    Added initalization of PWM. This code was compiled into the library. Note, RB3 is off limits unless it is being connected to the IR LED transistor setup

--- pic16f88init.c
+++ pic16f88init.c
@@ -1,5 +1,6 @@
 #include "pic16f88Init.h"
 #include <pic16f88.h>
+#include "pwm.h"
 void initPIC16F88()
 {
     OSCCON = 0b01101110;
@@ -8,4 +9,7 @@ void initPIC16F88()
     ANSEL = 0;
     PORTA = 0;
     PORTB = 0;
+
+    openPWM(25); //Sets the pwm to 38kHz
 }
+
